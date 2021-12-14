e3ffda683533c021ab80863058a51b1ba68e6e22
Arthur Vercruysse <arthur.vercruysse@persgroep.net>
2021-12-14 13:17:57 +0100

    even faster

--- days/14.c
+++ days/14.c
@@ -7,6 +7,9 @@
 
 #define SIZE 700
 
+int INDICES[256];
+int ind_c = 0;
+
 short shorten(char alpha, char beta) {
   return ((short)(alpha) << 5) | (short)(beta);
 }
@@ -18,6 +21,7 @@ void longen(short in, char *alpha, char *beta) {
 
 char parseInput(const char *loc, unsigned long pairs[SIZE], char map[SIZE]) {
   FILE *f = openFile(loc);
+  ind_c = 0;
 
   char buf[256] = {0};
 
@@ -33,6 +37,7 @@ char parseInput(const char *loc, unsigned long pairs[SIZE], char map[SIZE]) {
   while (fgets(buf, 9, f)) {
     short pair = shorten(buf[0] - 'A', buf[1] - 'A');
     map[pair] = buf[6] - 'A';
+    INDICES[ind_c++] = pair;
   }
 
   fclose(f);
@@ -42,16 +47,14 @@ char parseInput(const char *loc, unsigned long pairs[SIZE], char map[SIZE]) {
 void expand(char map[SIZE], unsigned long pairs1[SIZE],
             unsigned long pairs2[SIZE]) {
   char alpha, beta;
-  for (short i = 0; i < SIZE; i++) {
-    if (pairs1[i]) {
-      char insert = map[i];
-      longen(i, &alpha, &beta);
+  for (int i = 0, idx = INDICES[i]; i < ind_c; idx = INDICES[++i]) {
+    char insert = map[idx];
+    longen(idx, &alpha, &beta);
 
-      short one = shorten(alpha, insert);
-      short two = shorten(insert, beta);
-      pairs2[one] += pairs1[i];
-      pairs2[two] += pairs1[i];
-    }
+    short one = shorten(alpha, insert);
+    short two = shorten(insert, beta);
+    pairs2[one] += pairs1[idx];
+    pairs2[two] += pairs1[idx];
   }
 }
 
@@ -61,11 +64,9 @@ unsigned long calcOutput(unsigned long pairs[SIZE], int last_char) {
 
   numbers[last_char - 'A'] = 1;
 
-  for (unsigned int i = 0; i < SIZE; i++) {
-    if (pairs[i]) {
-      unsigned int a =  (0x1f & i);
-      numbers[a] += pairs[i];
-    }
+  for (int i = 0, idx = INDICES[i]; i < ind_c; idx = INDICES[++i]) {
+      unsigned int a = (0x1f & idx);
+      numbers[a] += pairs[idx];
   }
 
   for (int i = 0; i < 256; i++) {
