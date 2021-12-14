155881ef355edb89a5abea1c0d0ae463cd3b1fad
Arthur Vercruysse <arthur.vercruysse@persgroep.net>
2021-12-14 11:58:57 +0100

    part 2

--- days/14.c
+++ days/14.c
@@ -7,35 +7,33 @@
 
 #define SIZE 256000
 
+short shorten(char alpha, char beta) {
+  return ((short)(alpha - 'A') << 5) | (short)(beta - 'A');
+}
+
+void longen(short in, char *alpha, char *beta) {
+  *alpha = 'A' + (0x1f & (in >> 5));
+  *beta = 'A' + (0x1f & in);
+}
+
 char BUFFER[400] = {0};
-
-typedef struct Polymer Polymer;
-
-struct Polymer {
-  Polymer *next;
-  char value;
-};
-
-int poly_count;
-Polymer polymers[SIZE];
+char last_char;
 
 int cmpF(char *a, char *b) { return !strcmp(a, b); }
 
-Map *parseInput(const char *loc) {
+Map *parseInput(const char *loc, unsigned long pairs[1024]) {
   FILE *f = openFile(loc);
 
   Map *map = newMap((CmpF)cmpF);
 
   char buf[256] = {0};
   fscanf(f, "%s\n", buf);
-  printf("tart %s\n", buf);
-  poly_count = strlen(buf);
-  for (int i = 0; i < poly_count; i++) {
-    Polymer *p = polymers + i;
-    p->value = buf[i];
-    if (i < poly_count - 1)
-      p->next = &polymers[i + 1];
+  int len = strlen(buf);
+  for (int i = 1; i < len; i++) {
+    short pair = shorten(buf[i - 1], buf[i]);
+    pairs[pair]++;
   }
+  last_char = buf[len - 1];
 
   int i = 0;
   char c = '\0';
@@ -52,42 +50,47 @@ Map *parseInput(const char *loc) {
   return map;
 }
 
-void expand(Map *map) {
+void expand(Map *map, unsigned long pairs1[1024], unsigned long pairs2[1024]) {
   char buffer[3] = {0};
-  Polymer *first = polymers;
-  Polymer *second = first->next;
+  char alpha, beta;
+  for (short i = 0; i < 1024; i++) {
+    if (pairs1[i]) {
+      longen(i, &alpha, &beta);
 
-  int current_size = poly_count;
-  for (int i = 0; i < current_size - 1; i++) {
-    buffer[0] = first->value;
-    buffer[1] = second->value;
-    char insert = (char)(long)mapGet(map, buffer);
+      buffer[0] = alpha;
+      buffer[1] = beta;
+      char insert = (char)(long)mapGet(map, buffer);
 
-    if (insert) {
-      Polymer *new = polymers + poly_count;
-      poly_count++;
-      first->next = new;
-      new->next = second;
-      new->value = insert;
+      if (insert) {
+        short one = shorten(alpha, insert);
+        short two = shorten(insert, beta);
+        pairs2[one] += pairs1[i];
+        pairs2[two] += pairs1[i];
+      } else {
+        printf("oh no\n");
+        pairs2[i] = pairs1[i];
+      }
     }
-
-    first = second;
-    second = first->next;
   }
 }
 
-int calcOutput() {
-  int numbers[256] = {0};
-  Polymer *current = polymers;
-  while (current) {
-    numbers[current->value]++;
-    current = current->next;
+unsigned long calcOutput(unsigned long pairs[1024]) {
+  unsigned long numbers[256] = {0};
+  char a, b;
+  unsigned long min = ~0;
+  unsigned long max = 0;
+  numbers[(int) last_char] = 1;
+
+  for (int i = 0; i < 1024; i++) {
+    if (pairs[i]) {
+      longen(i, &a, &b);
+      numbers[(int)a] += pairs[i];
+    }
   }
-  int min = 1000000000;
-  int max = 0;
 
   for (int i = 0; i < 256; i++) {
     if (numbers[i]) {
+
       if (numbers[i] > max)
         max = numbers[i];
       if (numbers[i] < min)
@@ -99,16 +102,34 @@ int calcOutput() {
 }
 
 void part1(const char *inputLocation) {
-  Map *m = parseInput(inputLocation);
+  unsigned long pairs1[1024] = {0};
+  unsigned long pairs2[1024] = {0};
+  Map *m = parseInput(inputLocation, pairs1);
 
-  for (int i = 0; i < 10; i++)
-    expand(m);
-  printf("part 1: %d\n", calcOutput());
+  for (int i = 0; i < 10; i++) {
+    expand(m, pairs1, pairs2);
+    memmove(pairs1, pairs2, sizeof(long) * 1024);
+    memset(pairs2, 0, sizeof(long) * 1024);
+  }
+  printf("part 1: %ld\n", calcOutput(pairs1));
 
   freeMap(m, 0, 0);
 }
 
-void part2(const char *inputLocation) { printf("part 2: "); }
+void part2(const char *inputLocation) {
+  unsigned long pairs1[1024] = {0};
+  unsigned long pairs2[1024] = {0};
+  Map *m = parseInput(inputLocation, pairs1);
+
+  for (int i = 0; i < 40; i++) {
+    expand(m, pairs1, pairs2);
+    memmove(pairs1, pairs2, sizeof(long) * 1024);
+    memset(pairs2, 0, sizeof(long) * 1024);
+  }
+  printf("part 2: %ld\n", calcOutput(pairs1));
+
+  freeMap(m, 0, 0);
+}
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
