0f60b24fea2eb85fda440c6cd6e5982939d264a1
Arthur Vercruysse <arthur.vercruysse@persgroep.net>
2021-12-14 12:33:31 +0100

    cleanup

--- days/14.c
+++ days/14.c
@@ -5,92 +5,71 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE 256000
+#define SIZE 700
 
 short shorten(char alpha, char beta) {
-  return ((short)(alpha - 'A') << 5) | (short)(beta - 'A');
+  return ((short)(alpha) << 5) | (short)(beta);
 }
 
 void longen(short in, char *alpha, char *beta) {
-  *alpha = 'A' + (0x1f & (in >> 5));
-  *beta = 'A' + (0x1f & in);
+  *alpha = (0x1f & (in >> 5));
+  *beta = (0x1f & in);
 }
 
-char BUFFER[400] = {0};
-char last_char;
-
-int cmpF(char *a, char *b) { return !strcmp(a, b); }
-
-Map *parseInput(const char *loc, unsigned long pairs[1024]) {
+char parseInput(const char *loc, unsigned long pairs[SIZE], char map[SIZE]) {
   FILE *f = openFile(loc);
 
-  Map *map = newMap((CmpF)cmpF);
-
   char buf[256] = {0};
+
   fscanf(f, "%s\n", buf);
   int len = strlen(buf);
   for (int i = 1; i < len; i++) {
-    short pair = shorten(buf[i - 1], buf[i]);
+    short pair = shorten(buf[i - 1] - 'A', buf[i] - 'A');
     pairs[pair]++;
   }
-  last_char = buf[len - 1];
 
-  int i = 0;
-  char c = '\0';
-  while (fgets(&BUFFER[i * 3], 3, f)) {
-    fgets(buf, 5, f);
-    c = fgetc(f);
-    fgetc(f);
-    mapAdd(map, &BUFFER[i * 3], (void *)(long)c);
-    i++;
+  char last_char = buf[0];
+
+  while (fgets(buf, 9, f)) {
+    short pair = shorten(buf[0] - 'A', buf[1] - 'A');
+    map[pair] = buf[6] - 'A';
   }
 
   fclose(f);
-
-  return map;
+  return last_char;
 }
 
-void expand(Map *map, unsigned long pairs1[1024], unsigned long pairs2[1024]) {
-  char buffer[3] = {0};
+void expand(char map[SIZE], unsigned long pairs1[SIZE],
+            unsigned long pairs2[SIZE]) {
   char alpha, beta;
-  for (short i = 0; i < 1024; i++) {
+  for (short i = 0; i < SIZE; i++) {
     if (pairs1[i]) {
+      char insert = map[i];
       longen(i, &alpha, &beta);
 
-      buffer[0] = alpha;
-      buffer[1] = beta;
-      char insert = (char)(long)mapGet(map, buffer);
-
-      if (insert) {
-        short one = shorten(alpha, insert);
-        short two = shorten(insert, beta);
-        pairs2[one] += pairs1[i];
-        pairs2[two] += pairs1[i];
-      } else {
-        printf("oh no\n");
-        pairs2[i] = pairs1[i];
-      }
+      short one = shorten(alpha, insert);
+      short two = shorten(insert, beta);
+      pairs2[one] += pairs1[i];
+      pairs2[two] += pairs1[i];
     }
   }
 }
 
-unsigned long calcOutput(unsigned long pairs[1024]) {
+unsigned long calcOutput(unsigned long pairs[SIZE], int last_char) {
   unsigned long numbers[256] = {0};
-  char a, b;
-  unsigned long min = ~0;
-  unsigned long max = 0;
-  numbers[(int) last_char] = 1;
+  unsigned long min = ~0, max = 0;
 
-  for (int i = 0; i < 1024; i++) {
+  numbers[last_char - 'A'] = 1;
+
+  for (unsigned int i = 0; i < SIZE; i++) {
     if (pairs[i]) {
-      longen(i, &a, &b);
-      numbers[(int)a] += pairs[i];
+      unsigned int a =  (0x1f & i);
+      numbers[a] += pairs[i];
     }
   }
 
   for (int i = 0; i < 256; i++) {
     if (numbers[i]) {
-
       if (numbers[i] > max)
         max = numbers[i];
       if (numbers[i] < min)
@@ -102,33 +81,31 @@ unsigned long calcOutput(unsigned long pairs[1024]) {
 }
 
 void part1(const char *inputLocation) {
-  unsigned long pairs1[1024] = {0};
-  unsigned long pairs2[1024] = {0};
-  Map *m = parseInput(inputLocation, pairs1);
+  unsigned long pairs1[SIZE] = {0};
+  unsigned long pairs2[SIZE] = {0};
+  char map[SIZE] = {0};
+  char last_char = parseInput(inputLocation, pairs1, map);
 
   for (int i = 0; i < 10; i++) {
-    expand(m, pairs1, pairs2);
-    memmove(pairs1, pairs2, sizeof(long) * 1024);
-    memset(pairs2, 0, sizeof(long) * 1024);
+    expand(map, pairs1, pairs2);
+    memmove(pairs1, pairs2, sizeof(long) * SIZE);
+    memset(pairs2, 0, sizeof(long) * SIZE);
   }
-  printf("part 1: %ld\n", calcOutput(pairs1));
-
-  freeMap(m, 0, 0);
+  printf("part 1: %ld\n", calcOutput(pairs1, last_char));
 }
 
 void part2(const char *inputLocation) {
-  unsigned long pairs1[1024] = {0};
-  unsigned long pairs2[1024] = {0};
-  Map *m = parseInput(inputLocation, pairs1);
+  unsigned long pairs1[SIZE] = {0};
+  unsigned long pairs2[SIZE] = {0};
+  char map[SIZE] = {0};
+  char last_char = parseInput(inputLocation, pairs1, map);
 
   for (int i = 0; i < 40; i++) {
-    expand(m, pairs1, pairs2);
-    memmove(pairs1, pairs2, sizeof(long) * 1024);
-    memset(pairs2, 0, sizeof(long) * 1024);
+    expand(map, pairs1, pairs2);
+    memmove(pairs1, pairs2, sizeof(long) * SIZE);
+    memset(pairs2, 0, sizeof(long) * SIZE);
   }
-  printf("part 2: %ld\n", calcOutput(pairs1));
-
-  freeMap(m, 0, 0);
+  printf("part 2: %ld\n", calcOutput(pairs1, last_char));
 }
 
 int main(int argc, char *argv[]) {
